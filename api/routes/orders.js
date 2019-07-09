ccd14c3d9c270bfc34155c1689fc03a6a998bc6e
gauravsaxena1997 <gauravsaxena.cs@gmail.com>
2019-07-09 17:17:00 +0530

    body-parser, handling CORS

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -7,8 +7,13 @@ router.get('/',(req,res,next)=>{
     });
 });
 router.post('/',(req,res,next)=>{
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
     res.status(200).json({
-        message: 'Handling POST requests to /orders'
+        message: 'Order created.',
+        order: order
     });
 });
 router.get('/:orderId',(req,res,next)=>{
--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested_With, Content-Type, Accept, Authorization');
+    if (req.method==='OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT, GET, POST, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
+
 
 app.use((req,res,next)=>{
     const error = new Error('Not Found')
