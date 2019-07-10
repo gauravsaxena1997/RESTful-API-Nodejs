e3b82ef44018957aa00633ec122aab5b02682328
gauravsaxena1997 <gauravsaxena.cs@gmail.com>
2019-07-10 11:08:23 +0530

    update mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -45,13 +45,24 @@ router.get('/:productId',(req,res,next)=>{
         .catch(err=>{
             console.log(err);
             res.status(500).json({error: err});
-        })
+        });
 });
 router.patch('/:productId',(req,res,next)=>{
-    res.status(200).json({
-        message: 'Updated Successfully.',
-        id: req.params.productId
-    });
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({_id: id},{$set:updateOps})
+        .exec()
+        .then(result=>{
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({error: err});
+        });
 });
 router.delete('/:productId',(req,res,next)=>{
     const id = req.params.productId;
